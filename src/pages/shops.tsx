e2b9dc3b2a9222357ea480e23f780b6c5ed503cc
raykhona <raykhonarasulova@gmail.com>
2024-02-11 02:10:10 +0500

    perf:some+++

--- src/pages/shops.tsx
+++ src/pages/shops.tsx
@@ -1,83 +1,16 @@
-import React, { useState } from 'react';
-import { Button, Form, Input, Modal, Popconfirm, Table } from 'antd';
+import React from 'react';
+import { Button, Popconfirm, Table } from 'antd';
 import { useList } from 'modules/shops/hooks';
-import { Update } from 'modules/shops/shop';
-import { values } from 'lodash';
-import { IEntity } from 'modules/shops/types';
 
 const Shops: React.FC = () => {
   const { isLoading, shops, refetch } = useList();
-
-  const [state, setState] = useState<{ isModalOpen: boolean; shopdata: IEntity.Shop | null }>({
-    isModalOpen: false,
-    shopdata: null
-  });
-
-  const addShop = () => {
-    setState(prev => ({ ...prev, isModalOpen: true }));
-  };
-
-  const edit = (id: any) => {
-    // setState(prev => ({ ...prev, isModalOpen: true }));
-    // console.log(`[EDIT] = ${id}`);
-    // const shopdata = shops.filter(shop => shop.id === id);
-    // console.log(shops);
-    // console.log(shopdata[0]);
-    // setState(prev => ({ ...prev, shopdata: shopdata[0] }));
-    // console.log(state.shopdata);
-    // // const update = Update({id,...shopdata});
-    // refetch();
-  };
-
-  const modalStyles = {
-    header: {
-      borderLeft: `5px solid`,
-      borderRadius: 0,
-      paddingInlineStart: 5
-    },
-    mask: {
-      backdropFilter: 'blur(10px)'
-    },
-    oncancel: {
-      display: 'none'
-    }
-  };
 
   return (
     <>
       <div className="flex justify-between">
         <h2>Shops</h2>
-        <Button onClick={() => addShop()}>Add</Button>
+        <Button>Add</Button>
       </div>
-      <>
-        <Modal
-          title="Basic Modal"
-          open={state.isModalOpen}
-          onOk={() => setState(prev => ({ ...prev, isModalOpen: false }))}
-          onCancel={() => setState(prev => ({ ...prev, isModalOpen: false }))}
-          styles={modalStyles}
-        >
-          <Form
-            onFinish={() => {
-              console.log(values);
-            }}
-          >
-            <Form.Item label="Title" name="title">
-              <Input defaultValue="title" />
-            </Form.Item>
-            <Form.Item label="Location" name="location">
-              <Input defaultValue="location" />
-            </Form.Item>
-            <Form.Item label="phone" name="phone">
-              <Input defaultValue="phone" />
-            </Form.Item>
-            <Form.Item label="number" name="number">
-              <Input defaultValue="number" />
-            </Form.Item>
-          </Form>
-        </Modal>
-      </>
-
       <Table
         loading={isLoading}
         dataSource={shops}
@@ -106,9 +39,9 @@ const Shops: React.FC = () => {
           {
             title: 'Actions',
             dataIndex: 'id',
-            render: (id, record, index) => (
+            render: id => (
               <Button.Group>
-                <Button onClick={() => edit(id)}>Edit</Button>
+                <Button onClick={() => console.log(`[EDIT] = ${id}`)}>Edit</Button>
                 <Popconfirm
                   title="Delete the shop"
                   description="Are you sure to delete this shop?"
