d97a5c158f0258ca59687f54bb8d5f835ce28e49
raykhona <raykhonarasulova@gmail.com>
2024-02-11 14:17:10 +0500

    perf:shop.ts

--- src/modules/shops/shop.ts
+++ src/modules/shops/shop.ts
@@ -2,33 +2,29 @@ import { http } from 'services';
 import { IApi, IEntity, IForm } from './types';
 import { config } from 'config';
 
-export async function List() {
+export async function List(): Promise<IEntity.Shop[]> {
   const shops: IEntity.Shop[] = await http.get(`/shops`);
   // const data: IEntity.Shop[] = await res.json(); // JSON string -> convert object
 
   return shops;
 }
 
-export async function Single(shopId: string) {
+export async function Single(shopId: string): Promise<IEntity.Shop> {
   const shop: IEntity.Shop = await http.get(`/shops/${shopId}`);
   return shop;
 }
 
-export async function Create(values: IForm.Add) {
+export async function Create(values: IForm.Add): Promise<any> {
   const res = await http.post('/shops', JSON.stringify(values));
   return res;
 }
-// export async function Delete(shopId: string) {
-//   const res = await http.delete(`/shops/${shopId}`);
-//   return res;
-// }
 
+export const Delete = async ({ token, id }: IApi.Delete.Request): Promise<any> => {
+  const res = await http.delete(`/shops/${id}`, { headers: { [config.api.tokenKEY]: token } });
+  return res;
+};
 
-export const Delete = ( { token,id }: IApi.Delete.Request) =>
-  http.delete(`/shops/${id}`, { headers: { [config.api.tokenKEY]: token } });
-
-export async function Update({ id, ...body }: IForm.Update) {
+export async function Update({ id, ...body }: IForm.Update): Promise<any> {
   const res = await http.patch(`/shops/${id}`, JSON.stringify(body));
-
   return res;
 }
